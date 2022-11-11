9d3a3cd8dc10d5d1359feee839bb40857b023f89
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-11-11 12:16:55 +0000

    Created an external middleware function to make code more DRY

--- routes/users.router.js
+++ routes/users.router.js
@@ -8,32 +8,26 @@ const { Show, User } = require("../models");
 
 const { param, validationResult } = require("express-validator");
 
+async function checkUser(req, res, next) {
+  const user = await User.findByPk(req.params.id);
+
+  if (user === null) {
+    res.status(404).send("User is not found");
+  } else {
+    next();
+  }
+}
+
 usersRouter.get("/", async (req, res) => {
   const users = await User.findAll();
 
   res.send(users);
 });
 
-usersRouter.get(
-  "/:id",
-  param("id").custom(async (value, { req }) => {
-    return User.findByPk(req.params.id).then((user) => {
-      if (user === null) {
-        return Promise.reject("User does not exist");
-      }
-    });
-  }),
-  async (req, res) => {
-    const errors = validationResult(req).array({ onlyFirstError: true });
+usersRouter.get("/:id", checkUser, async (req, res) => {
+  const user = await User.findByPk(req.params.id);
 
-    if (errors.length > 0) {
-      res.send(errors[0].msg);
-    } else {
-      const user = await User.findByPk(req.params.id);
-
-      res.send(user);
-    }
-  }
-);
+  res.send(user);
+});
 
 module.exports = usersRouter;
