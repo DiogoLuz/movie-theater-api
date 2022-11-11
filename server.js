e06a20dfaff13a4b40f5ec5404263600dcb7da22
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-11-11 17:40:07 +0000

    Imported shows router and added it to Express server

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const PORT = 3000;
 
 const usersRouter = require("./routes/users.router");
 
+const showsRouter = require("./routes/shows.router");
+
+app.use(express.json());
+
 app.use("/users", usersRouter);
+
+app.use("/shows", showsRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
